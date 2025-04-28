1cdce0b5bf2c35e0cf63bfe066b0805b9ee786e3
sameu <77215384+Kwisma@users.noreply.github.com>
2025-04-28 14:52:56 +0800

    Create mihomo.sh

---
+++ scripts/mihomo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 遍历 rukle 目录下所有子目录
+find "$base_dir" -type f -name '*IP.yaml' | while read -r src_file; do
+    # 定义目标文件路径
+    # 假设目标文件与源文件同名，只是格式不同
+    target_file="${src_file%.yaml}.converted.mrs"  # 可以修改目标文件的后缀名格式
+
+    # 执行转化指令
+    ./mihomo convert-ruleset ipcidr yaml "$src_file" "$target_file"
+
+    # 打印已转换的文件信息
+    echo "Converted: $src_file -> $target_file"
+done
